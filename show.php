7f79f4bf674bda4ccbdc7c03366842d709739464
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-11-26 23:36:03 +0700

    Update show.php

--- show.php
+++ show.php
@@ -33,7 +33,7 @@ $res = mysqli_query($conn, 'SELECT * FROM abc');
             <a class="nav-link" href="show.php">Show</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="index.php">Insert</a>
+            <a class="nav-link" href="sad.html">Insert</a>
         </ul>
       </div>
     </div>
